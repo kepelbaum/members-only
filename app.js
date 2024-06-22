271e383c1c206f75d9712a89c26055c72dbddc31
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-06-21 20:45:04 -0400

    fix: add memorystore

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const MemoryStore = require('memorystore')(session);
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
@@ -50,8 +51,16 @@ const Post = mongoose.model('Post', PostSchema);
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
-
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  resave: false,
+  secret: 'keyboard cat',
+  saveUninitialized: true 
+}))
+// app.use(session({ secret: "cats", resave: false, }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
